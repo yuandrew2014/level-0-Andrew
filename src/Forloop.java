b508787ccd369ca40598f064524adf2763fd0bff
test <league@WTS15.attlocal.net>
2017-05-13 15:56:06 -0700

    sdhgkfguyasdgadskuygfhfdgsdfgfasvytfgewgtisefuh da bosds

---
+++ src/Forloop.java
@@ -0,0 +1,15 @@
+
+public class Forloop {
+public static void main(String[] args) {
+	int Count = 1;
+	int hi = 0;
+	for (int i = 13; i < 999; i++) {
+		System.out.println(hi);
+		System.out.println(Count);
+	hi=hi+Count;
+	Count=Count+hi;
+	
+}
+	}
+}
+
